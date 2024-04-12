f001043d6a1becbd83e41f63f6dd9cb44b7c80b7
rihem1999 <rihem.farjallah1999@gmail.com>
2024-04-12 23:56:12 +0100

    profile list added

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,33 +2,40 @@ import {useState}from 'react'
 import { assets } from '../../assets/assets';
 import './Nav.css';
 import { Link } from 'react-router-dom';
-import logout from '../../assets/logOut.png';
 import profile from '../../assets/profile.png';
+import ProfilePopup from '../ProfilePopUp/ProfilePopUp';
 // eslint-disable-next-line react/prop-types
 const Nav = ({ setShowLogin }) => {
   const[menu,setMenu]=useState("home");
+  const [profilePopUp, setProfilePopUp] = useState(false);
   // check if user is already logged in:
   const authToken = localStorage.getItem('authToken');
-  const handleLogOut = () => {
+ /* const handleLogOut = () => {
         localStorage.removeItem('authToken');
   }
+  */
+ const handleProfilePopUp =  () =>{
+  console.log("in handle profile popUp");
+  profilePopUp ? setProfilePopUp(false) : setProfilePopUp(true);
+ }
   return (
     <div className='navbar' id='nav'>
         <img src={assets.logo} alt="Logo" className='logo'/>  
           <ul className="navbar-menu">
             <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}> Home</Link>
-            <li onClick={()=>setMenu("AdoptMe")}className={menu==="AdoptMe"?"active":""}>AdoptMe</li>
+            <li onClick={()=>setMenu("AdoptMe")}className={menu==="AdoptMe"?"active":""}>Adopt Me</li>
             <li onClick={()=>setMenu("Forum")}className={menu==="Forum"?"active":""}>Forum</li>
             <li onClick={()=>setMenu("Contacts")} className={menu==="Contacts"?"active":""}>Contacts</li>
-            <a href='#footer' onClick={()=>setMenu("AboutUs")}className={menu==="AboutUs"?"active":""}>AboutUs</a>
+            <a href='#footer' onClick={()=>setMenu("AboutUs")}className={menu==="AboutUs"?"active":""}>About Us</a>
           </ul>
           <div className='navbar-right'>
           {(authToken)?
            <div style={{display: "flex", flexDirection: "row"}}>
-            <img src={profile} alt="Profile"style={{width: 70, height: 70,}} />
-            <img src={logout} alt="logout" onClick={handleLogOut} style={{width: 70, height: 70, paddingLeft:10}} />
-
+            <img src={profile} alt="Profile"style={{width: 60, height: 60, cursor: "pointer"}} onClick={handleProfilePopUp} />
+         
           </div> : <button onClick={()=>setShowLogin(true)}>Sign in</button>}
+          {profilePopUp && 
+          <ProfilePopup />}
           
           </div>
     </div>
---
+++ src/components/ProfilePopUp/ProfilePopUp.jsx
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+import './ProfilePopUp.css';
+import profile from '../../assets/profil.png';
+import logout from '../../assets/logOut.png';
+import parameters from '../../assets/parametres.png';
+// eslint-disable-next-line react/prop-types
+const ProfilePopup = () => {
+
+  
+  
+  
+  
+  return (
+    <div className='profile-popup'>
+      <div className='profile-popup-container'>
+        <div className= 'profile-popup-items'>
+          <div className='profile-popup-item'>
+            <img src={profile} alt="" />
+            <span>Profile</span>
+            
+          </div>
+          <div className='divider'></div> {/* Divider */}
+          <div className='profile-popup-item'>
+            <img src={parameters} alt="" />
+            <span>Settings</span>
+            
+          </div>
+          <div className='divider'></div> {/* Divider */}
+          <div className='profile-popup-item'>
+            <img src={logout} alt="" />
+            <span>Logout</span>
+            
+          </div>
+        </div>
+         
+      </div>
+    </div>
+  )
+}
+
+export default ProfilePopup
